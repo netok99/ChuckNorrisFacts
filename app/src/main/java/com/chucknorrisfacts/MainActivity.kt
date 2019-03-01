8f91156f28558b82e0bd5c1dc752a693a136f845
Gildenio Neto <netok99@gmail.com>
2019-03-01 15:13:22 -0300

    Adição dos módulos search e network

--- app/src/main/java/com/chucknorrisfacts/MainActivity.kt
+++ app/src/main/java/com/chucknorrisfacts/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.example.chucknorrisfacts
+package com.chucknorrisfacts
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import com.chucknorrisfacts.search.SearchActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
         }
     }
 
---
+++ search/src/main/java/com/chucknorrisfacts/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.chucknorrisfacts.search
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
